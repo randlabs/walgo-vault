f7b0c0496eaafbd0e1297018e9a2fe82391af4f5
pipaman <pablo.yabo@gmail.com>
2020-09-11 12:49:51 -0300

    Format update JS

--- asa-tools.js
+++ asa-tools.js
@@ -1,54 +1,49 @@
-const algosdk = require ('algosdk');
+const algosdk = require('algosdk')
 
-async function createASA(algodClient, owner, totalSupply, decimals) {
-    try {
-        let params = await algodClient.getTransactionParams();
-		
-        let suggestedParams = {
-            "genesisHash": params.genesishashb64,
-            "genesisID": params.genesisID,
-            "firstRound": params.lastRound,
-            "lastRound": params.lastRound + 10,
-            "fee": params.minFee,
-            "flatFee": true
-        };
+async function createASA (algodClient, owner, totalSupply, decimals) {
+  try {
+    const params = await algodClient.getTransactionParams()
 
-        let createAssetTx = algosdk.makeAssetCreateTxnWithSuggestedParams(owner.addr, new Uint8Array(Buffer.from("Wrapped ALGO Asset", "utf8")), totalSupply, decimals,
-            false, owner.addr, owner.addr, owner.addr, owner.addr, "wALGO", "Wrapped ALGO", "https://stakerdao", undefined,
-            suggestedParams);
-        let createAssetTxSigned = createAssetTx.signTxn(owner.sk);
-        let createAssetTxSent = (await algodClient.sendRawTransaction(createAssetTxSigned));
-        return createAssetTxSent;
+    const suggestedParams = {
+      genesisHash: params.genesishashb64,
+      genesisID: params.genesisID,
+      firstRound: params.lastRound,
+      lastRound: params.lastRound + 10,
+      fee: params.minFee,
+      flatFee: true
     }
-    catch(e) {
-        if(e && e.error && e.error.message) {
-            console.log(e.error.message);
-        }
-        else {
-            console.log("Error: " + e);
-        }
+
+    const createAssetTx = algosdk.makeAssetCreateTxnWithSuggestedParams(owner.addr, new Uint8Array(Buffer.from('Wrapped ALGO Asset', 'utf8')), totalSupply, decimals,
+      false, owner.addr, owner.addr, owner.addr, owner.addr, 'wALGO', 'Wrapped ALGO', 'https://stakerdao', undefined,
+      suggestedParams)
+    const createAssetTxSigned = createAssetTx.signTxn(owner.sk)
+    const createAssetTxSent = (await algodClient.sendRawTransaction(createAssetTxSigned))
+    return createAssetTxSent
+  } catch (e) {
+    if (e && e.error && e.error.message) {
+      console.log(e.error.message)
+    } else {
+      console.log('Error: ' + e)
     }
+  }
 }
 
-
-async function destroyASA(algodClient, owner) {
-    try {
-        let assetDestroy = algosdk.makeAssetDestroyTxnWithSuggestedParams(owner.addr, new Uint8Array(0), 2654202, suggestedParams);
-        let assetDestroySigned = assetDestroy.signTxn(owner.sk);
-        let assetDestroySent = (await algodClient.sendRawTransaction(assetDestroySigned));
-        return assetDestroySent;
+async function destroyASA (algodClient, owner) {
+  try {
+    const assetDestroy = algosdk.makeAssetDestroyTxnWithSuggestedParams(owner.addr, new Uint8Array(0), 2654202, suggestedParams)
+    const assetDestroySigned = assetDestroy.signTxn(owner.sk)
+    const assetDestroySent = (await algodClient.sendRawTransaction(assetDestroySigned))
+    return assetDestroySent
+  } catch (e) {
+    if (e && e.error && e.error.message) {
+      console.log(e.error.message)
+    } else {
+      console.log('Error: ' + e)
     }
-    catch(e) {
-        if(e && e.error && e.error.message) {
-            console.log(e.error.message);
-        }
-        else {
-            console.log("Error: " + e);
-        }
-    }
+  }
 }
 
 module.exports = {
-	destroyASA: destroyASA,
-	createASA: createASA
-};
+  destroyASA: destroyASA,
+  createASA: createASA
+}
--- main.js
+++ main.js
@@ -1,41 +1,38 @@
-const algosdk = require('algosdk');
-const asaTools = require('./asa-tools');
+const algosdk = require('algosdk')
+const asaTools = require('./asa-tools')
 
-let algodClient;
+let algodClient
 
+function recoverManagerAccount () {
+  // Private key mnemonic: town memory type rapid ugly aim yard moon rocket lobster survey series mesh plate great seed company vote debris limb view motion label absorb swear
 
-function recoverManagerAccount() {
-    // Private key mnemonic: town memory type rapid ugly aim yard moon rocket lobster survey series mesh plate great seed company vote debris limb view motion label absorb swear
-    
-	// WALBWVI43IKS7YJJADD7WGE4C4TS3OTOJOEAVEWNXEJS6CXH7OJIVKKHME
-	const passphrase = "clay vast only enact sibling axis seven around drip cruise era alcohol police web planet increase winter exclude rain pyramid art alert tool absent pave";
-	let myAccount = algosdk.mnemonicToSecretKey(passphrase);
-	return myAccount;
+  // WALBWVI43IKS7YJJADD7WGE4C4TS3OTOJOEAVEWNXEJS6CXH7OJIVKKHME
+  const passphrase = 'clay vast only enact sibling axis seven around drip cruise era alcohol police web planet increase winter exclude rain pyramid art alert tool absent pave'
+  const myAccount = algosdk.mnemonicToSecretKey(passphrase)
+  return myAccount
 }
 
-async function setupClient() {
-	if (algodClient == null) {
-		const token = {
-		}
+async function setupClient () {
+  if (algodClient == null) {
+    const token = {
+    }
 
-		const server = "https://api.testnet.algoexplorer.io";
-		const port = "";
-		algodClient = new algosdk.Algod(token, server, port);
+    const server = 'https://api.testnet.algoexplorer.io'
+    const port = ''
+    algodClient = new algosdk.Algod(token, server, port)
+  } else {
+    return algodClient
+  }
 
-	} else {
-		return algodClient;
-	}
-
-	return algodClient;
+  return algodClient
 }
 
-async function main() {
-    let managerAccount = recoverManagerAccount();
+async function main () {
+  const managerAccount = recoverManagerAccount()
 
-    // ASA ID Testnet: 11870752
-    // let tx = await asaTools.createASA(algodClient, managerAccount, 9007199254740991, 6);
-    
+  // ASA ID Testnet: 11870752
+  // let tx = await asaTools.createASA(algodClient, managerAccount, 9007199254740991, 6);
 }
 
-setupClient();
-main();
+setupClient()
+main()
