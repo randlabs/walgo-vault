71db892421b127066696ec1ee583a6625ff1d115
pipaman <pablo.yabo@gmail.com>
2021-01-06 19:29:56 -0300

    Format

--- test/mocha-tools.js
+++ test/mocha-tools.js
@@ -1,38 +1,42 @@
-var expect = require("chai").expect;
+/* eslint-disable max-len */
+let expect = require("chai").expect;
 
 function errorText(err) {
-	let text =  err.error
+	let text = err.error;
 
 	if (err.text) {
-		if(err.text.message) {
-			text == err.text.message
+		if (err.text.message) {
+			text = err.text.message;
 		}
 		else {
-			text = err.text
+			text = err.text;
 		}
 	}
 	else if (err.message) {
-		text = err.message
+		text = err.message;
 	}
-	return text
+	return text;
 }
 
 function expectTxId(txId) {
+	// eslint-disable-next-line require-unicode-regexp
 	expect(txId).to.match(/[\dA-Z]{52}/);
 }
 
 function expectTEALReject(err) {
-	let text = errorText(err)
-	expect(text).to.match(/(?:TEAL runtime encountered err opcode|would result negative|transaction rejected by ApprovalProgram|rejected by logic|below min 100000)/)
+	let text = errorText(err);
+	// eslint-disable-next-line require-unicode-regexp
+	expect(text).to.match(/(?:TEAL runtime encountered err opcode|would result negative|transaction rejected by ApprovalProgram|rejected by logic|below min 100000)/);
 }
 
 function expectTEALRejectNonAdminError(err) {
-	let text = errorText(err)
-	expect(text).to.match(/(?:cannot fetch state)/)
+	let text = errorText(err);
+	// eslint-disable-next-line require-unicode-regexp
+	expect(text).to.match(/(?:cannot fetch state)/);
 }
 
-module.exports = { 
+module.exports = {
 	expectTxId,
 	expectTEALReject,
 	expectTEALRejectNonAdminError
-}
+};
