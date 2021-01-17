ecbecf0aee43c8fa3fb9e08e888536e76e20b9f2
pipaman <pablo.yabo@gmail.com>
2021-01-17 17:20:13 -0300

    Removed unnecessary file. Apply format to config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+/* eslint-disable global-require */
 /*************************************************************************
  *  [2018] - [2020] Rand Labs Inc.
  *  All Rights Reserved.
@@ -8,90 +9,95 @@
  * herein are proprietary to Rand Labs Inc.
  */
 
-const process = require('process')
-const path = require('path')
-const algosdk = require('algosdk')
+const process = require('process');
+const path = require('path');
+const algosdk = require('algosdk');
 
 // ------------------------------------------------------------------------------
-let settings = null
-let algodClient = null
-let addresses = {}
-let signatures = {}
+let settings = null;
+let algodClient = null;
+let addresses = {};
+let signatures = {};
 
 // ------------------------------------------------------------------------------
 function initialize() {
 	// setup the settings filename
-	let filename = 'settings'
+	let filename = 'settings';
 	for (let idx = 0; idx < process.argv.length; idx++) {
 		// eslint-disable-next-line security/detect-object-injection
 		if (process.argv[idx] == '--settings') {
 			if (idx + 1 >= process.argv.length) {
-				throw new Error('ERROR: Missing filename in "--settings" parameter.')
+				throw new Error('ERROR: Missing filename in "--settings" parameter.');
 			}
-			filename = process.argv[idx + 1]
+			filename = process.argv[idx + 1];
 		}
 	}
 
 	try {
-		filename = path.resolve(__dirname, '.', filename)
+		filename = path.resolve(__dirname, '.', filename);
 
-		settings = require(filename)
+		// eslint-disable-next-line security/detect-non-literal-require
+		settings = require(filename);
 
-		recoverAccounts()
-		setupClient()
+		recoverAccounts();
+		setupClient();
 
-		settings.algodClient = algodClient
-		settings.signatures = signatures
-		settings.addresses = addresses
-	} catch (err) {
-		throw new Error('ERROR: Unable to load settings file.')
+		settings.algodClient = algodClient;
+		settings.signatures = signatures;
+		settings.addresses = addresses;
+		settings.admin = !settings.noAdmin;
+
+	}
+	catch (err) {
+		throw new Error('ERROR: Unable to load settings file.');
 	}
 
-	settings.base_dir = path.dirname(filename)
+	settings.base_dir = path.dirname(filename);
 	if (!settings.base_dir.endsWith(path.sep)) {
-		settings.base_dir += path.sep
+		settings.base_dir += path.sep;
 	}
 
-	return settings
+	return settings;
 }
 
 function recoverAccounts () {
-	for(let i = 0; i < 20; i++) {
-		if(settings["account" + i]) {
-			addresses[i] = settings["account" + i].publicKey
-			if(settings["account" + i].privateKey) {
-				signatures[addresses[i]] = algosdk.mnemonicToSecretKey(settings["account" + i].privateKey)
+	for (let i = 0; i < 20; i++) {
+		if (settings["account" + i]) {
+			addresses[i] = settings["account" + i].publicKey;
+			if (settings["account" + i].privateKey) {
+				signatures[addresses[i]] = algosdk.mnemonicToSecretKey(settings["account" + i].privateKey);
 			}
 		}
 		else {
 			break;
 		}
 	}
-	if(settings["minterAccount"]) {
-		settings.minterAddress = settings["minterAccount"].publicKey
-		signatures[settings.minterAddress] = algosdk.mnemonicToSecretKey(settings["minterAccount"].privateKey)
+	if (settings.minterAccount) {
+		settings.minterAddress = settings.minterAccount.publicKey;
+		signatures[settings.minterAddress] = algosdk.mnemonicToSecretKey(settings.minterAccount.privateKey);
 	}
-	if(settings["dispenserAccount"]) {
-		settings.dispenserAddress = settings["dispenserAccount"].publicKey
-		signatures[settings.dispenserAddress] = algosdk.mnemonicToSecretKey(settings["dispenserAccount"].privateKey)
+	if (settings.dispenserAccount) {
+		settings.dispenserAddress = settings.dispenserAccount.publicKey;
+		signatures[settings.dispenserAddress] = algosdk.mnemonicToSecretKey(settings.dispenserAccount.privateKey);
 	}
-	if(settings["clearStateAttackAccount"]) {
-		settings.clearStateAttackAddr = settings["clearStateAttackAccount"].publicKey
-		signatures[settings.clearStateAttackAddr] = algosdk.mnemonicToSecretKey(settings["clearStateAttackAccount"].privateKey)
+	if (settings.clearStateAttackAccount) {
+		settings.clearStateAttackAddr = settings.clearStateAttackAccount.publicKey;
+		signatures[settings.clearStateAttackAddr] = algosdk.mnemonicToSecretKey(settings.clearStateAttackAccount.privateKey);
 	}
 }
 
-async function setupClient() {
-	if (algodClient == null) {
-		algodClient = new algosdk.Algodv2(settings.algodClient.apiToken, settings.algodClient.server, settings.algodClient.port)
-	} else {
-		return algodClient
+function setupClient() {
+	if (!algodClient) {
+		algodClient = new algosdk.Algodv2(settings.algodClient.apiToken, settings.algodClient.server, settings.algodClient.port);
+	}
+	else {
+		return algodClient;
 	}
 
-	return algodClient
+	return algodClient;
 }
 
 // ------------------------------------------------------------------------------
 module.exports = {
 	initialize
-}
+};
